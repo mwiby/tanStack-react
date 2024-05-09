e093a4c251f0676a9e9d4a70f135fdbe5b919269
Markus Byremo <markuswb77@gmail.com>
2024-05-09 14:22:19 +0200

    v1 impl tanstack query all authors from backend/kotlin rest api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,64 @@
+import axios from 'axios'
+import {
+  useQuery,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+//import AuthorView from './view/AuthorView.jsx'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-function App() {
+const queryClient = new QueryClient()
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className='container'>
+      <ListAuthors />
+      </div>
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
+  )
+};
+
+const useAuthors = () => {
+  return useQuery({
+    queryKey: ['authors'],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        'http://localhost:8080/authors',
+      )
+      return data
+    },
+  })
+};
+
+const ListAuthors = () => {
+
+  const { status, data, error, isFetching } = useAuthors()
 
   return (
-    <>
+    <div>
+      <h1>Authors</h1>
       <div>
-        <h1>Vite + React + Tanstack</h1>
+        {status === 'pending' ? (
+          'Loading...'
+        ) : status === 'error' ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          <>
+            <div>
+              {data.map((author: { id: number; name: string }) => (
+                <p key={author.id}>
+                <span>{author.name}</span>
+                </p>
+              ))}
+            </div>
+            <div>{isFetching ? 'Background Updating...' : ' '}</div>
+          </>
+        )}
       </div>
-    </>
+    </div>
   )
 }
 
